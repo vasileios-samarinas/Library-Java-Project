5616be91411d0055b153e4cbfb60ca935b994d95
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-03-24 19:12:02 +0000

    Initial Commit

---
+++ src/main/java/Library.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Library {
+
+    private ArrayList<Book>books;
+    private int capacity;
+
+    public Library(int capacity){
+        this.books=new ArrayList<Book>();
+        this.capacity=capacity;
+    }
+
+
+    public int countBooks() {
+        return this.books.size();
+    }
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before(){
+        library=new Library(30);
+        book=new Book("Harry Potter and the Goblet of Fire","J.K. Rowling","Fantasy");
+    }
+
+    @Test
+    public void libraryStartsEmpty(){
+        assertEquals(0,library.countBooks());
+    }
+
+
+
+}
